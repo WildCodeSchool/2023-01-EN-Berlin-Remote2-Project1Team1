7bed15e2909993d04f4529cee37e51707f2dec58
denigogov <122295314+denigogov@users.noreply.github.com>
2023-02-08 15:42:12 +0100

    add some future

---
+++ swiperCommands.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: false,
+  },
+  loop: true,
+});
