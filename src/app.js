c552286d1eff130876b29fdc23da445f1862b17e
denigogov <122295314+denigogov@users.noreply.github.com>
2023-02-16 17:31:11 +0100

    add Iframe js

--- src/app.js
+++ src/app.js
@@ -1,27 +1,27 @@
 "use strict";
 // ADD POPUP WINDOW
 
-//  Step 1. We define the variables you can check the class in html document
-const overlay = document.querySelector(".overlay");
-const variable = document.querySelector(".brejj");
-const popup = document.querySelector(".popup");
-const iframe = document.querySelector("iframe");
+// //  Step 1. We define the variables you can check the class in html document
+// const overlay = document.querySelector(".overlay");
+// const variable = document.querySelector(".brejj");
+// const popup = document.querySelector(".popup");
+// const iframe = document.querySelector("iframe");
 
-//  Step 2.
-// We add function and we take the documents and add the HIDDEN CLASS => Property TOGGLE what means in this case
-//  (if on Iframe or popup ... element  is a class hidden REMOVE IT, and if there if no class HIDDEN add to the element)
-// you can check from inspect in browser when you click on the button how the class disappear from the place where is added :D
-const openClose = function () {
-  iframe.classList.toggle("hidden");
-  popup.classList.toggle("hidden");
-  overlay.classList.toggle("hidden");
-};
+// //  Step 2.
+// // We add function and we take the documents and add the HIDDEN CLASS => Property TOGGLE what means in this case
+// //  (if on Iframe or popup ... element  is a class hidden REMOVE IT, and if there if no class HIDDEN add to the element)
+// // you can check from inspect in browser when you click on the button how the class disappear from the place where is added :D
+// const openClose = function () {
+//   iframe.classList.toggle("hidden");
+//   popup.classList.toggle("hidden");
+//   overlay.classList.toggle("hidden");
+// };
 
-// STEP 3.
-// We add addEventListener on click on the element we want to add click to happen  and we pass as argument the function without ()
-// I added also on overlay (popup background) eventListener becouse when someone click on the background to exit from the popUp :D
-variable.addEventListener("click", openClose);
-overlay.addEventListener("click", openClose);
+// // STEP 3.
+// // We add addEventListener on click on the element we want to add click to happen  and we pass as argument the function without ()
+// // I added also on overlay (popup background) eventListener becouse when someone click on the background to exit from the popUp :D
+// variable.addEventListener("click", openClose);
+// overlay.addEventListener("click", openClose);
 
 // add popUp picture
 const overlay1 = document.querySelector(".overlay");
@@ -32,7 +32,7 @@ findYourMuse.addEventListener("click", function () {
   popUpPicture1.classList.toggle("hidden");
 });
 
-const youCanGoTheDistance = document.querySelector(".youCanGoTheDistance");
+const youCanGoTheDistance = document.querySelector(".youCanGoTheDistanceTitle");
 const popUpPicture2 = document.querySelector(".popUpPicture2");
 
 youCanGoTheDistance.addEventListener("click", function () {
@@ -75,3 +75,37 @@ const obsOption = {
 
 const headerObserver = new IntersectionObserver(arrowFunc, obsOption);
 headerObserver.observe(header);
+
+///////////////////////////////////////////////////////////////////////////////////
+
+const bubles = document.querySelectorAll(".circle");
+const overlay = document.querySelector(".overlay");
+const popup = document.querySelector(".popup");
+const iframe = document.querySelector("iFrame");
+
+const openClose = function () {
+  popup.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+};
+
+const iFrameSection = function (target) {
+  bubles.forEach((buble) => {
+    buble.addEventListener("click", function (e) {
+      if (e.target.textContent === target) {
+        openClose();
+        iframe.src = `https://en.wikipedia.org/wiki/${target}`;
+        iframe.classList.toggle("hidden");
+      }
+
+      if (e.target.textContent) {
+        overlay.addEventListener("click", openClose);
+      }
+    });
+  });
+};
+iFrameSection("HTML");
+iFrameSection("CSS");
+iFrameSection("JavaScript");
+iFrameSection("Node.JS");
+iFrameSection("PHP");
+iFrameSection("Java");
